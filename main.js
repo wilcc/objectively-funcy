1e11cf914a7b5a5ad1517b2ecbd0f017391871c7
Wilson Chen <wilson.chen@codeimmersives.com>
2020-03-26 12:03:24 -0400

    finish

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
 function getFirstName(person) {
+  return person.firstName
 }
 
 function getLastName(person) {
+  return person.lastName
 }
 
 function getFullName(person) {
+  return person.firstName +' '+ person.lastName
 }
 
 function setFirstName(person, name) {
+  person.firstName = name
 }
 
 function setAge(person, age) {
+  person.age = age
 }
 
 function giveBirthday(person) {
+  if(person.age === undefined){
+    return person.age = 1
+  } else 
+  return person.age+=1
 }
 
 function marry(person1, person2) {
+  person1.married = true
+  person2.married = true
+  person1.spouseName = getFullName(person2)
+  person2.spouseName = getFullName(person1)
+
 }
 
 function divorce(person1, person2) {
+  person1.married = false
+  person2.married = false
+  delete person1.spouseName
+  delete person2.spouseName
 }
 
 module.exports = {
